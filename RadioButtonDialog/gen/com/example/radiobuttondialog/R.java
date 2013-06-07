bcbdc56e6a7e8b4867c41ae043333353c4ece8e2
sunjie <sunjie@sunjie-System-Product-Name>
2013-06-07 11:41:14 +0800

    修改

--- RadioButtonDialog/gen/com/example/radiobuttondialog/R.java
+++ RadioButtonDialog/gen/com/example/radiobuttondialog/R.java
@@ -8,22 +8,25 @@
 package com.example.radiobuttondialog;
 
 public final class R {
+    public static final class array {
+        public static final int ms=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f060001;
-        public static final int EditText01=0x7f060000;
+        public static final int Button01=0x7f070001;
+        public static final int EditText01=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int listmultiple=0x7f040002;
-        public static final int ok=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int listmultiple=0x7f050002;
+        public static final int ok=0x7f050001;
     }
     public static final class style {
         /** 
@@ -47,10 +50,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
