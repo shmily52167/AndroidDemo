8ca8b1fc97a286eed038365b26940921ccf03beb
sunjie <651241596@qq.com>
2013-06-26 10:44:16 +0800

    提交

--- AlarmManager/src/com/itcast/alarm/MainActivtiy.java
+++ AlarmManager/src/com/itcast/alarm/MainActivtiy.java
@@ -14,6 +14,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -21,12 +24,16 @@ import com.example.alarmmanager.R;
 
 public class MainActivtiy extends Activity {
 	Calendar c=Calendar.getInstance();
+	private CheckBox checkBox;
 	final int  DIALOG_TIME=0;
 	AlarmManager am;
+	boolean flag=false;//是否只执行一次
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		checkBox=(CheckBox) this.findViewById(R.id.checkbox_alarm);
+		checkBox.setOnCheckedChangeListener(new CheckListener());
 		am=(AlarmManager) getSystemService(Context.ALARM_SERVICE);
 		Button btn=(Button) findViewById(R.id.btn);
 		btn.setOnClickListener(new OnClickListener() {
@@ -37,6 +44,16 @@ public class MainActivtiy extends Activity {
 				showDialog(DIALOG_TIME);
 			}
 		});
+	}
+	private final class CheckListener implements OnCheckedChangeListener{
+
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView,
+				boolean isChecked) {
+			flag=!isChecked;
+			System.out.println(isChecked);
+		}
+		
 	}
 	@Override
 	protected Dialog onCreateDialog(int id) {
@@ -55,8 +72,13 @@ public class MainActivtiy extends Activity {
 					c.set(Calendar.MILLISECOND, 0);//设置闹钟的毫秒数	
 					Intent intent=new Intent(getApplicationContext(),AlarmRecriver.class);
 					PendingIntent pi=PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
-//					am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+5*1000, pi);
-					am.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);
+					if (flag) {
+						am.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);
+						am.cancel(pi);
+
+					}else {
+						am.setRepeating(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), 5*1000, pi);
+					}
 					Toast.makeText(getApplicationContext(), "闹钟设置成功", Toast.LENGTH_SHORT).show();
 					}
 			}, c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE),true);
